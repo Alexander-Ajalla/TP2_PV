475cb3626f40ffef3a08311af7e3506fc7df6d01
alex._unu <alexader.ajalla404@gmail.com>
2024-04-28 17:18:00 -0300

    Clase main: agregando menú de opciones

--- TP2_AjallaAlexander/src/ar/edu/unju/ejercicio1/main/Main.java
+++ TP2_AjallaAlexander/src/ar/edu/unju/ejercicio1/main/Main.java
@@ -1,10 +1,43 @@
 package ar.edu.unju.ejercicio1.main;
 
+import java.util.List;
+import java.util.Scanner;
+
+import ar.edu.unju.ejercicio1.model.Producto;
+
 public class Main {
-
+	private static Scanner scanner;
+	private static List<Producto> productos;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int option = 0;
+			do {
+				System.out.println("-------------------------");
+				System.out.println("1 - Crear Productos");
+				System.out.println("2 - Mostrar Productos");
+				System.out.println("3 - Modificar producto");
+				System.out.println("4 - Salir");
+				System.out.println("-------------------------");
+				System.out.println("Ingrese una opcion: ");
+				option = scanner.nextInt();
+				scanner.nextLine();
+				switch (option) {
+				case 1: 
+					System.out.println("Crear Productos");
+					break;
+				case 2:
+					System.out.println("Mostrar Productos");
+					break;
+				case 3:
+					System.out.println("Modificar Producto");
+					break;
+				case 4: 
+					System.out.println("Fin del programa");
+				default:
+					System.out.println("Opcion incorrecta");
+					break;
+				} 
+			} while(option !=4);
 	}
-
 }
--- TP2_AjallaAlexander/src/ar/edu/unju/ejercicio1/model/Producto.java
+++ TP2_AjallaAlexander/src/ar/edu/unju/ejercicio1/model/Producto.java
@@ -7,4 +7,61 @@ public class Producto {
 	OrigenFabricacion origen;
 	Categoria categoria;
 	
+	public Producto() {}
+
+	public Producto(int producto, String descripcion, Double precioU, OrigenFabricacion origen, Categoria categoria) {
+		super();
+		this.producto = producto;
+		this.descripcion = descripcion;
+		this.precioU = precioU;
+		this.origen = origen;
+		this.categoria = categoria;
+	}
+
+	public int getProducto() {
+		return producto;
+	}
+
+	public void setProducto(int producto) {
+		this.producto = producto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecioU() {
+		return precioU;
+	}
+
+	public void setPrecioU(Double precioU) {
+		this.precioU = precioU;
+	}
+
+	public OrigenFabricacion getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(OrigenFabricacion origen) {
+		this.origen = origen;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	@Override
+	public String toString() {
+		return "Producto [producto=" + producto + ", descripcion=" + descripcion + ", precioU=" + precioU + ", origen="
+				+ origen + ", categoria=" + categoria + "]";
+	}
+
 }
